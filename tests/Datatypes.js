ecd793504a9d9c6777b246a7aeb0f4f18c731d56
PackialakshmiN <laks12feb@gmail.com>
2025-11-25 15:26:09 +0530

    Add files via upload

---
+++ tests/Datatypes.js
@@ -0,0 +1,21 @@
+var firstName='Packia';
+console.log(firstName); // "Packia"
+console.log(typeof firstName); // "string" 
+
+var mobileNumber = 99887766556;
+console.log(mobileNumber); // 25
+console.log(typeof mobileNumber); // "number"
+
+var isAutomation = true;
+console.log(isAutomation); // true
+console.log(typeof isAutomation); // "boolean"
+
+var companyName = "Bell Electronics";
+console.log(companyName); // "Bell Electronics"
+console.log(typeof companyName); // "string"
+
+var hasPlaywright;
+console.log(hasPlaywright); // undefined
+console.log(typeof hasPlaywright); //   "undefined"
+
+
